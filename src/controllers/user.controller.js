c5db47e514bb9770f6e73615fb9097e12d60befe
shubhamcypher <shubhamdevo23@gmail.com>
2024-02-25 01:21:33 +0530

    update user details,avatar and coverImage

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -153,7 +153,7 @@ const loginUser = asyncHandler( async (req,res) => {
 } )
 
 //dekho samasya toh ye hai ki login and register mei toh req.body se user le lete hai but log out k wqt user ko form thodi denge.....so no req.body
-// toh hum krenge ye ki
+// toh hum krenge ye ki ek middleware design kar denge verifyJWT usse kya hoga ki during routing login user ka access req.user mei chala jyga ab use wo har koi access kar lega jo v middleware ke bad route krega
 const logoutUser = asyncHandler(async (req,res) => {
     User.findByIdAndUpdate(
         req.user._id,
@@ -222,4 +222,133 @@ const refreshAccessToken = asyncHandler(async(req,res)=>{
     }
 })
 
-export {registerUser,loginUser,logoutUser,refreshAccessToken}
+const changeCurrentPassword = asyncHandler(async(req,res)=>{
+    const {oldPassword, newPassword} = req.body
+
+    const user = await User.findById(req.user?._id)
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+    if (!isPasswordCorrect) {
+        throw new ApiError(400,"Invalid old Password")
+    }
+
+    user.password = newPassword
+    await user.save({validateBeforeSave:false})
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            {},
+            "Password changed Successfully"
+        )
+    )
+
+})
+
+const getCurrentUser = asyncHandler(async(req,res)=>{
+    return res
+    .status(200)
+    .json(200,req.user,"user fetched successfully")
+})
+
+const updateAccountDetails = asyncHandler(async(req,res)=>{
+    const {username,email} = req.body
+
+    if (!username || !email) {
+        throw new ApiError(400,"All fields are required")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                fullName,
+                email
+            }
+        },
+        {new:true}
+        ).select("-password")
+
+        return res
+        .status(200)
+        .json(new ApiResponse(200,user,"Account details updated successfully"))
+})
+
+const updateUserAvatar = asyncHandler(async(req,res)=>{
+    const avatarLocalPath = req.file?.path
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400,"Avatar file is missing")
+    }
+        const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+        if (!avatar.url) { //cloudinary sends "" if any issue occurs while uploading any file on cloudinary
+            throw new ApiError(400,"Error while uploading avatar file")
+        }
+
+        const user = await User.findByIdAndUpdate
+        (
+            req.user._id,
+            {
+                $set:{
+                    avatar:avatar.url
+                }
+            },
+            {
+                new: true
+            }
+        
+        ).select("-password")
+
+        return res
+        .status(
+            200,
+            new ApiResponse(
+                200,
+                user,
+                "Avatar image updated successfully"
+            )
+        )
+})
+
+const updateUserCoverImage = asyncHandler(async(req,res)=>{
+    const CoverImageLocalPath = req.file?.path
+
+    if (!CoverImageLocalPath) {
+        throw new ApiError(400,"Cover image file is missing")
+    }
+        const CoverImage = await uploadOnCloudinary(CoverImageLocalPath)
+
+        if (!CoverImage.url) { //cloudinary sends "" if any issue occurs while uploading any file on cloudinary
+            throw new ApiError(400,"Error while uploading Cover image file")
+        }
+
+        const user = await User.findByIdAndUpdate
+        (
+            req.user._id,
+            {
+                $set:{
+                    CoverImage:CoverImage.url
+                }
+            },
+            {
+                new: true
+            }
+        
+        ).select("-password")
+
+        return res
+        .status(
+            200,
+            new ApiResponse(
+                200,
+                user,
+                "Cover image updated successfully"
+            )
+        )
+})
+
+export {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,
+        updateAccountDetails,updateUserAvatar,updateUserCoverImage}
